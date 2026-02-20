f66ad23adeb448bd4ce66673aa374e7829630843
Tomoya Iwata <iwata.tomoya@classmethod.jp>
2026-02-20 17:06:37 +0900

    test

--- save-vectors.sh
+++ save-vectors.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
+
 set -eo pipefail
 
-# 入力JSONの配列を受け取る
-# 例: ./script.sh '[{"text":"bbb"},{"text":"aaa"}]'
-INPUT_JSON="$1"
+PAGES="$1"
+VECTOR_BUCKET="$2"
+VECTOR_INDEX="$3"
 
-echo "$INPUT_JSON"
 # JSONの要素数を取得
-ELEMENT_COUNT=$(echo "$INPUT_JSON" | jq 'length')
+ELEMENT_COUNT=$(echo "$PAGES" | jq 'length')
 echo "要素数: $ELEMENT_COUNT"
 
 # 要素数だけループ
 for i in $(seq 0 $((ELEMENT_COUNT - 1))); do
+    
+
+    PAGE_NAME=$(echo "$PAGES" | jq -r ".[$i].page_name")
     echo "----------------------------------------"
-    echo "処理中: インデックス $i"
+    echo "処理中: $PAGE_NAME"
 
-    # JSON要素を取得
-    TEXT=$(echo "$INPUT_JSON" | jq -c ".[$i].text")
-    TITLE=$(echo "$INPUT_JSON" | jq -c ".[$i].title")
-    echo "要素: $TEXT"
+    PAGE_CONTENT=$(cat "$PAGE_NAME.md")
+    
 
-    # Bedrockへの入力ファイルを生成
-    echo "{\"inputText\": $TEXT}" > file.json
-
-    # Bedrockでベクトル化
+    jq -n --arg text "$PAGE_CONTENT" '{"inputText": $text}' > file.json
     aws bedrock-runtime invoke-model \
         --model-id amazon.titan-embed-text-v2:0 \
         --body fileb://file.json \
@@ -32,33 +30,29 @@ for i in $(seq 0 $((ELEMENT_COUNT - 1))); do
         --accept application/json \
         output.json
 
-    # jqでベクトルを取得
     jq -c '.embedding' output.json > vector_data.json
-
 
     # アップロード用のベクトルJSONを作成
     cat > vector_upload.json << EOF
 [
   {
-    "key": ${TITLE},
+    "key": "${PAGE_NAME}",
     "data": {
       "float32": $(cat vector_data.json)
     },
     "metadata": {
-      "source_text": ${TEXT}
+      "source_text": "${PAGE_CONTENT}"
     }
   }
 ]
 EOF
-    echo "アップロード用JSON生成完了"
-
-    # S3にベクトルを登録
     aws s3vectors put-vectors \
         --vector-bucket-name "$VECTOR_BUCKET" \
         --index-name "$VECTOR_INDEX" \
         --vectors file://vector_upload.json
 
-    echo "S3へのベクトル登録完了: ${TITLE}"
+    echo "S3へのベクトル登録完了: ${PAGE_NAME}"
+    echo "----------------------------------------"
 done
 
 echo "========================================"
